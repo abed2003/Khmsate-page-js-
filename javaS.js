f1eb5429d9e96fcf6220c19e251880943070cbe1
Abed <abed.2003.ama@gmail.com>
2025-11-06 01:48:43 +0200

    first v

---
+++ javaS.js
@@ -0,0 +1,233 @@
+let categories  = [
+    { name: 'تصاميم سوشيال ميديا', title: 789 ,
+        subcategories :[
+            { name: 'تصاميم سوشيال ميديا', title: 22 },
+            { name: 'تصاميم سوشيال ميديا', title: 22 },
+            { name: 'تصاميم سوشيال ميديا', title: 22 },
+            { name: 'تصاميم سوشيال ميديا', title: 22 },
+            { name: 'تصاميم سوشيال ميديا', title: 22 },
+            { name: 'تصاميم سوشيال ميديا', title: 22 },
+            { name: 'تصاميم سوشيال ميديا', title: 22 },
+    ]},
+    { name: 'تصاميم سوشيال ميديا', title: 789 ,
+        subcategories :[
+            { name: 'تصاميم سوشيال ميديا', title: 22 },
+            { name: 'تصاميم سوشيال ميديا', title: 22 },
+            { name: 'تصاميم سوشيال ميديا', title: 22 },
+            { name: 'تصاميم سوشيال ميديا', title: 22 },
+            { name: 'تصاميم سوشيال ميديا', title: 22 },
+            { name: 'تصاميم سوشيال ميديا', title: 22 },
+            { name: 'تصاميم سوشيال ميديا', title: 22 },
+    ]
+    },
+    { name: 'تصاميم سوشيال ميديا', title: 1 },
+    { name: 'تصاميم سوشيال ميديا', title: 2 },
+    { name: 'تصاميم سوشيال ميديا', title: 3 },
+    { name: 'تصاميم سوشيال ميديا', title: 4 },
+    { name: 'تصاميم سوشيال ميديا', title: 789 },
+    { name: 'تصاميم سوشيال ميديا', title: 789 },
+    { name: 'تصاميم سوشيال ميديا', title: 789 },
+    { name: 'تصاميم سوشيال ميديا', title: 789 },
+    { name: 'تصاميم سوشيال ميديا', title: 789 },
+    { name: 'تصاميم سوشيال ميديا', title: 5 },
+    { name: 'تصاميم سوشيال ميديا', title: 789 },
+    { name: 'تصاميم سوشيال ميديا', title: 789 },
+    { name: 'تصاميم سوشيال ميديا', title: 789 },
+    { name: 'تصاميم سوشيال ميديا', title: 6 },
+    { name: 'تصاميم سوشيال ميديا', title: 789 },
+    { name: 'تصاميم سوشيال ميديا', title: 789 },
+    { name: 'تصاميم سوشيال ميديا', title: 789 },
+    { name: 'تصاميم سوشيال ميديا', title: 789 },
+    { name: 'تصاميم سوشيال ميديا', title: 789 },
+    { name: 'تصاميم سوشيال ميديا', title: 7 },
+    { name: 'تصاميم سوشيال ميديا', title: 789 },
+    { name: 'تصاميم سوشيال ميديا', title: 789 },
+    { name: 'تصاميم سوشيال ميديا', title: 789 },
+    { name: 'تصاميم سوشيال ميديا', title: 8 },
+    { name: 'تصاميم سوشيال ميديا', title: 789 },
+    { name: 'تصاميم سوشيال ميديا', title: 789 },
+    { name: 'تصاميم سوشيال ميديا', title: 9 },
+    { name: 'تصاميم سوشيال ميديا', title: 0 },
+]
+
+let mainContent = [
+    { imgSrc: 'https://khamsat.hsoubcdn.com/images/services/372354/77f6f294a998686e8379a8c9a9ee64cd.jpg', title: ' تصميم هوية بصرية احترافية ' ,
+        desc: ' تصميم / تصميم هوية بصرية ' , price: '5$'  , rateSayler : 'بائع ممير' , saylerStatus : 'متواجد حالياً' },
+    { imgSrc: 'https://khamsat.hsoubcdn.com/images/services/372354/77f6f294a998686e8379a8c9a9ee64cd.jpg', title: ' تصميم هوية بصرية احترافية ' ,
+        desc: ' تصميم / تصميم هوية بصرية ' , price: '50$' , rateSayler : 'بائع نشيط'  , saylerStatus : 'متواجد حالياً'},
+    { imgSrc: 'https://khamsat.hsoubcdn.com/images/services/679802/fd56a795b5e7aa0658ff4498e48c5f02.jpg?s=small', title: ' تصميم هوية بصرية احترافية ' ,
+        desc: ' تصميم / تصميم هوية بصرية ' , price: '12$' , rateSayler : 'بائع جديد' , saylerStatus : 'هوية موثقة'},
+    { imgSrc: 'https://khamsat.hsoubcdn.com/images/services/1970127/05c5085680af11cf6fd992617589fd64_thumb.jpg', title: ' تصميم هوية بصرية احترافية ' ,
+        desc: ' تصميم / تصميم هوية بصرية ' , price: '15$' , rateSayler : 'بائع ممير' , saylerStatus : 'متواجد حالياً'},
+    { imgSrc: 'https://khamsat.hsoubcdn.com/images/services/1764073/abdedb19e2343a88014abf352b0c850a.jpg?s=small', title: ' تصميم هوية بصرية احترافية ' ,
+        desc: ' تصميم / تصميم هوية بصرية ' , price: '25$' , rateSayler : 'بائع ممير' , saylerStatus : 'متواجد حالياً'},
+    { imgSrc: 'https://khamsat.hsoubcdn.com/images/services/2403506/2c04d3620489f4407de936e987518a87.jpg?s=small', title: ' تصميم هوية بصرية احترافية ' ,
+        desc: ' تصميم / تصميم هوية بصرية ' , price: '25$' , rateSayler : 'بائع ممير' , saylerStatus : 'متواجد حالياً'},
+    { imgSrc: 'https://khamsat.hsoubcdn.com/images/services/2403506/2c04d3620489f4407de936e987518a87.jpg?s=small', title: ' تصميم هوية بصرية احترافية ' ,
+        desc: ' تصميم / تصميم هوية بصرية ' , price: '55$'  , rateSayler : 'بائع نشيط', saylerStatus : 'متواجد حالياً'},
+    { imgSrc: 'https://khamsat.hsoubcdn.com/images/services/1623953/de2a917dcbbae5dd376df79715c57ec1.jpg?s=small', title: ' تصميم هوية بصرية احترافية ' ,
+        desc: ' تصميم / تصميم هوية بصرية ' , price: '10$'  , rateSayler : 'بائع نشيط', saylerStatus : 'متواجد حالياً'},
+    { imgSrc: 'https://khamsat.hsoubcdn.com/images/services/2522304/97deca37a57c8ab0f0354fd00d4a1db5.jpg?s=small', title: ' تصميم هوية بصرية احترافية ' ,
+        desc: ' تصميم / تصميم هوية بصرية ' , price: '12$'  , rateSayler : 'بائع نشيط', saylerStatus : 'متواجد حالياً'},
+    { imgSrc: 'https://khamsat.hsoubcdn.com/images/services/834502/6705b2f7d1e6f4e4f20679b6bc9aba8f.jpg?s=small', title: ' تصميم هوية بصرية احترافية ' ,
+        desc: ' تصميم / تصميم هوية بصرية ' , price: '19$'  , rateSayler : 'بائع نشيط' , saylerStatus : 'هوية موثقة'},
+    { imgSrc: 'https://khamsat.hsoubcdn.com/images/services/40269/fc950a8fe66bf6cd082c4a5e1bc5099e.jpg?s=small', title: ' تصميم هوية بصرية احترافية ' ,
+        desc: ' تصميم / تصميم هوية بصرية ' , price: '20$'  , rateSayler : 'بائع جديد' , saylerStatus : 'هوية موثقة'},
+    // { imgSrc: 'https://khamsat.hsoubcdn.com/images/services/372354/77f6f294a998686e8379a8c9a9ee64cd.jpg', title: ' تصميم هوية بصرية احترافية ' ,
+    //     desc: ' تصميم / تصميم هوية بصرية ' , price: '5$'  },
+    // { imgSrc: 'https://khamsat.hsoubcdn.com/images/services/372354/77f6f294a998686e8379a8c9a9ee64cd.jpg', title: ' تصميم هوية بصرية احترافية ' ,
+    //     desc: ' تصميم / تصميم هوية بصرية ' , price: '50$'  },
+    { imgSrc: 'https://khamsat.hsoubcdn.com/images/services/679802/fd56a795b5e7aa0658ff4498e48c5f02.jpg?s=small', title: ' تصميم هوية بصرية احترافية ' ,
+        desc: ' تصميم / تصميم هوية بصرية ' , price: '12$' , rateSayler : 'بائع جديد' , saylerStatus : 'متواجد حالياً'},
+    { imgSrc: 'https://khamsat.hsoubcdn.com/images/services/1970127/05c5085680af11cf6fd992617589fd64_thumb.jpg', title: ' تصميم هوية بصرية احترافية ' ,
+        desc: ' تصميم / تصميم هوية بصرية ' , price: '15$' , rateSayler : 'بائع ممير' , saylerStatus : 'متواجد حالياً'},
+    { imgSrc: 'https://khamsat.hsoubcdn.com/images/services/1764073/abdedb19e2343a88014abf352b0c850a.jpg?s=small', title: ' تصميم هوية بصرية احترافية ' ,
+        desc: ' تصميم / تصميم هوية بصرية ' , price: '25$'  , rateSayler : 'بائع جديد' , saylerStatus : 'هوية موثقة'},
+    { imgSrc: 'https://khamsat.hsoubcdn.com/images/services/2403506/2c04d3620489f4407de936e987518a87.jpg?s=small', title: ' تصميم هوية بصرية احترافية ' ,
+        desc: ' تصميم / تصميم هوية بصرية ' , price: '25$'  , rateSayler : 'بائع جديد', saylerStatus : 'متواجد حالياً'},
+    { imgSrc: 'https://khamsat.hsoubcdn.com/images/services/2522304/97deca37a57c8ab0f0354fd00d4a1db5.jpg?s=small', title: ' تصميم هوية بصرية احترافية ' ,
+        desc: ' تصميم / تصميم هوية بصرية ' , price: '55$'  , rateSayler : 'بائع جديد', saylerStatus : 'متواجد حالياً'},
+    { imgSrc: 'https://khamsat.hsoubcdn.com/images/services/1623953/de2a917dcbbae5dd376df79715c57ec1.jpg?s=small', title: ' تصميم هوية بصرية احترافية ' ,
+        desc: ' تصميم / تصميم هوية بصرية ' , price: '10$' , rateSayler : 'بائع ممير' , saylerStatus : 'متواجد حالياً'},
+    { imgSrc: 'https://khamsat.hsoubcdn.com/images/services/2522304/97deca37a57c8ab0f0354fd00d4a1db5.jpg?s=small', title: ' تصميم هوية بصرية احترافية ' ,
+        desc: ' تصميم / تصميم هوية بصرية ' , price: '12$'  , rateSayler : 'بائع جديد' , saylerStatus : 'هوية موثقة'},
+    { imgSrc: 'https://khamsat.hsoubcdn.com/images/services/834502/6705b2f7d1e6f4e4f20679b6bc9aba8f.jpg?s=small', title: ' تصميم هوية بصرية احترافية ' ,
+        desc: ' تصميم / تصميم هوية بصرية ' , price: '19$'  , rateSayler : 'بائع جديد' , saylerStatus : 'هوية موثقة'},
+    { imgSrc: 'https://khamsat.hsoubcdn.com/images/services/40269/fc950a8fe66bf6cd082c4a5e1bc5099e.jpg?s=small', title: ' تصميم هوية بصرية احترافية ' ,
+        desc: ' تصميم / تصميم هوية بصرية ' , price: '20$'  , rateSayler : 'بائع نشيط' , saylerStatus : 'هوية موثقة'},
+    { imgSrc: 'https://khamsat.hsoubcdn.com/images/services/40269/fc950a8fe66bf6cd082c4a5e1bc5099e.jpg?s=small', title: ' تصميم هوية بصرية احترافية ' ,
+        desc: ' تصميم / تصميم هوية بصرية ' , price: '20$' , rateSayler : 'بائع ممير' , saylerStatus : 'هوية موثقة'},
+                
+            ]
+            
+            
+        let container = document.getElementById("categoryContainer");
+        let contentImg = document.getElementById("services-Container"); 
+
+
+
+function SetCategories() {
+
+for (let i = 0; i < categories.length; i++) {
+    const categoryDiv = document.createElement("div");
+    categoryDiv.className = "category";
+    let catiegoryName = document.createElement("a");
+    let catiegoryTarget = document.createElement("a");
+    catiegoryName.innerText = categories[i].name;
+    catiegoryTarget.innerText = categories[i].title;
+    catiegoryTarget.className = "category-title";
+    // catiegoryTarget.href = "#";
+    categoryDiv.appendChild(catiegoryName);
+    categoryDiv.appendChild(catiegoryTarget);
+
+    container.appendChild(categoryDiv);
+    if ( !categories[i].subcategories ) continue;
+    for (let j = 0; j < categories[i].subcategories.length; j++) {
+        const subDiv = document.createElement("div");
+        subDiv.className = "subcategory";
+        let subName = document.createElement("a");
+        let subTarget = document.createElement("a");
+        subTarget.className = "subcategory-title";
+        subName.innerText = categories[i].subcategories[j].name;
+        subTarget.href = "#";
+        subTarget.innerText = categories[i].subcategories[j].title;
+        subDiv.appendChild(subName);
+        subDiv.appendChild(subTarget);
+        categoryDiv.appendChild(subDiv);
+    }
+}
+}
+SetCategories();
+
+
+function generateMainContent() { 
+    contentImg.innerHTML = '' ;
+    for(let i =0; i< mainContent.length; i++) {
+    let  containerContent = `
+    <div class="col-lg-4 col-md-4 col-sm-6 col-xs-6 ">
+        <div class="thumnal text-right thumnal-content">
+            <div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="false"> 
+                <div class="carousel-inner">
+                    <div class="item active">
+                        <img src="${mainContent[i].imgSrc}" alt="Avatar" class="img-responsive">
+                    </div>
+                </div>
+            </div>
+            <div class="caption text-right">    
+                <h4>${mainContent[i].title}</h4>
+                <p>${mainContent[i].desc}</p>
+                <div class="price-box"> 
+                    <span style="color: #fa9747;"><i class="fa-solid fa-star "></i><i
+                        class="fa-solid fa-star "></i><i class="fa-solid fa-star "></i><i
+                        class="fa-solid fa-star "></i><i class="fa-solid fa-star "></i>
+                    </span>
+                    <button class="btn btn-primary btn-sm" onclick="generateMainContent(${i})">اشتري الآن</button>
+                    <button class="btn btn-danger btn-sm" onclick="DeleteItem(${i})"> حذف العنصر</button>
+                    <br>
+                    <span class="price">${mainContent[i].price}</span>
+                </div>
+            </div>  
+        </div>
+    </div>`;
+    contentImg.innerHTML += containerContent;
+    }
+}
+generateMainContent ();
+
+function DeleteItem(index) {
+    let confirmDelete = confirm("هل أنت متأكد من حذف هذا العنصر؟");
+    if (confirmDelete){
+        alert("تم حذف العنصر بنجاح.");
+        mainContent.splice(index, 1);
+        generateMainContent();
+    }
+    else { 
+            generateMainContent();
+        }
+}
+
+function GetBlurQualefications(Qualefication) {
+    if (Qualefication === null ){
+        generateMainContent ();
+    }
+    // filterContent();
+    contentImg.innerHTML = '' ;
+    for(let i =0; i< mainContent.length; i++) {
+        if ( mainContent[i].rateSayler === Qualefication || mainContent[i].saylerStatus === Qualefication ) {
+        let  containerContent = `
+        <div class="col-lg-4 col-md-4 col-sm-6 col-xs-6 ">
+            <div class="thumnal text-right thumnal-content">
+                <div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="false"> 
+                    <div class="carousel-inner">
+                        <div class="item active">
+                            <img src="${mainContent[i].imgSrc}" alt="Avatar" class="img-responsive">
+                        </div>
+                    </div>
+                </div>
+                <div class="caption text-right">    
+                    <h4>${mainContent[i].title}</h4>
+                    <p>${mainContent[i].desc}</p>
+                    <div class="price-box"> 
+                        <span style="color: #fa9747;"><i class="fa-solid fa-star "></i><i
+                            class="fa-solid fa-star "></i><i class="fa-solid fa-star "></i><i
+                            class="fa-solid fa-star "></i><i class="fa-solid fa-star "></i>
+                        </span>
+                        <button class="btn btn-primary btn-sm" onclick="generateMainContent(${i})">اشتري الآن</button>
+                        <button class="btn btn-danger btn-sm" onclick="DeleteItem(${i})"> حذف العنصر</button>
+                        <br>
+                        <span class="price">${mainContent[i].price}</span>
+                    </div>
+                </div>  
+            </div>
+        </div>`;
+        contentImg.innerHTML += containerContent;
+        }
+        else {
+            continue;
+        }
+    }
+}
+
+
+
+
